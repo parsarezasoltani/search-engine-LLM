39798c5bc122baae51a0068f9f58afd4ad3ae952
parsarezasoltani <50063644+parsarezasoltani@users.noreply.github.com>
2025-11-23 14:02:04 +0330

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
 
     tools=[search,arxiv,wiki]
 
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+    search_agent=initialize_agent(tools,llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
 
     with st.chat_message("assistant"):
         st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
