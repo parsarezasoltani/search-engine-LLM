f84b58db691b8e333e57cda54f9098143e6121ad
parsarezasoltani <50063644+parsarezasoltani@users.noreply.github.com>
2025-11-24 09:03:53 +0330

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     st.chat_message("user").write(prompt)
 
     #llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
-    llm=ChatOllama(model="llama3.1:8b",streaming=True)
-#from langchain_ollama import ChatOllama
+    #llm=ChatOllama(model="llama3.1:8b",streaming=True)
+    #from langchain_ollama import ChatOllama
 
     llm = ChatOllama(
         model="llama3.2",
@@ -60,6 +60,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
         response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
         st.session_state.messages.append({'role':'assistant',"content":response})
         st.write(response)
+
 
 
 
